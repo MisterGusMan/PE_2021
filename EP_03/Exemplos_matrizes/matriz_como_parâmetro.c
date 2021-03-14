d9d107cfbdeaf02a0aec7c4cac65048369e546f6
Gustavo Mantovani Fonseca <manto.fon@gmail.com>
2021-03-14 19:37:16 -0300

    Delete EP_03 directory

--- EP_03/Exemplos_matrizes/matriz_como_parâmetro.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-// Para passar uma matriz como parâmetro em uma função, é necessário especificar ao menos a segunda dimensão:
-void funcao1(int n_colunas, int matriz[][n_colunas]);
-
-void funcao2(int n_colunas, int n_linhas, int matriz[n_linhas][n_colunas]);
